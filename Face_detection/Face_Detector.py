678540ff620731f0f858c92d6fad248143ce24a8
vithushanms <53653757+vithushanms@users.noreply.github.com>
2021-01-16 04:12:29 +0530

    Comments are added to the Face_Detectoe.py file

--- Face_detection/Face_Detector.py
+++ Face_detection/Face_Detector.py
@@ -1,18 +1,26 @@
 import cv2
 import random
 
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#loading the trained model into the application
+trained_face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+#Getting the default web-cam into a variable
 cam = cv2.VideoCapture(0)
 
 while True:
+    #Read the frames from your web-cam
     frame_read, frame = cam.read()
-    grayScaledImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face_cordinants = trained_face_data.detectMultiScale(frame)
+
+    #Extract the array of detected coordinates 
+    face_cordinants = trained_face_detector.detectMultiScale(frame)
+
+    #Draw the squire using the identified coordinates
     for (x,y,w,h) in face_cordinants :
         cv2.rectangle(frame, ( x, y),(x+ w , y+ h),(random.randrange(255),random.randrange(255),random.randrange(255)), 2)
 
+    #Start the GUI to open up the web-cam
     cv2.imshow('predictedimg', frame)
+    
     cv2.waitKey(1)
 
 
